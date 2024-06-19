56cb82f32f95df8dd90078c8994f0492ed36432b
polygon707070 <polygon707070@tutamail.com>
2024-06-19 17:23:27 +0300

    update config settings

---
+++ FrontEnd/src/app/manage-key-words/manage-key-words.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-key-words',
+  templateUrl: './manage-key-words.component.html',
+  styleUrls: ['./manage-key-words.component.css']
+})
+export class ManageKeyWordsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
