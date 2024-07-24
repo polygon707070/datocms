c0fa5134aa8aeab4b33d2d5c88fc61a3d86ab0d8
polygon707070 <polygon707070@tutamail.com>
2024-07-24 17:23:27 +0300

    Improved UX

---
+++ BackEnd/fieryLotos.Ports/Driven/QueryModel/UserPermissions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fieryLotos.Ports.Driven.QueryModel
+{
+    public class UserPermissions
+    {
+        public bool CanEditArticles { get; set; }
+        public bool CanEditTags { get; set; }
+        public bool CanEditKeyWords { get; set; }
+    }
+}
