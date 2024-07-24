c9760464e3e4150dc012597b6826ee2ed9a1e836
polygon707070 <polygon707070@tutamail.com>
2024-07-24 17:23:27 +0300

    update server config

---
+++ BackEnd/fieryLotos.Ports/Driven/QueryModel/LoginQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fieryLotos.Ports.Driven.QueryModel
+{
+    public class LoginQuery
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
