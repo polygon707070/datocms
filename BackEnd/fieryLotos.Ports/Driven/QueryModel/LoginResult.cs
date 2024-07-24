65f493e0b8eb415a02bc1e75aa9e4576515de260
polygon707070 <polygon707070@tutamail.com>
2024-07-24 17:23:27 +0300

    fix login issue

---
+++ BackEnd/fieryLotos.Ports/Driven/QueryModel/LoginResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fieryLotos.Ports.Driven.QueryModel
+{
+    public class LoginResult
+    {
+        public bool Success { get; set; }
+        public string ErrorMessage { get; set; }
+        public string Token { get; set; }
+        public UserPermissions Permissions { get; set; }
+    }
+}
